e65a95db9eab7e4e6467873f77711e6d181062ad
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-25 15:36:29 -0400

    enabling the likeing functionality

--- main.js
+++ main.js
@@ -3,7 +3,56 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const domLoaded = () => {
+  document.addEventListener('DOMContentLoaded', 
+  () => { 
+    hideError()
+    likeHandler()})
+}
 
+function likeHandler() {
+  const likeElements = document.querySelectorAll('.like')
+  console.log(likeElements)
+  for (like of likeElements) {
+    like.addEventListener('click', (e) => {
+      e.preventDefault()
+      //console.log(e.path)
+      const likedArticle = e.path[4].id
+      console.log(likedArticle)
+      const heart = document.getElementById(likedArticle).querySelector('ul').querySelector('span')
+      console.log(heart)
+      mimicServerCall().then(res => {
+        console.log(res)
+        if (res === 'Pretend remote server notified of action!') {
+          if (heart.className === 'like-glyph') {
+            heart.className = 'activated-heart'
+          }
+          else {
+            heart.className = 'like-glyph'
+          }
+        } else {
+          res => {
+            showError()
+          }
+          }
+        }
+      )
+    })
+  }}
+
+  const hideError = () => {
+      const modal = document.querySelector('#modal')
+      modal.className = 'hidden'
+  }
+
+  const showError = () => {
+    const modal = document.querySelector('#modal')
+    console.log(modal.className)
+    modal.className=''
+    console.log(modal.className)
+}
+
+domLoaded()
 
 
 
